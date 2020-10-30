b514d0fcbce6ab8766170ed6c28bc372fc9993b8
priest9494 <priest9494@yandex.ru>
2020-10-30 10:50:31 +0300

    Edit content recommendations

--- services/contentService.js
+++ services/contentService.js
@@ -1,14 +1,42 @@
 const getRecommend = require('../utils/content/recommend');
+const getFullInfo = require('../getFullInfo');
 
 const recommend = function(basket, avoidList) {
-    let recommendList = getRecommend(['Мэнку', 'Тенчун']);
-    let avoidRecommends = getRecommend(['ДаЛи']);
+    let recommendList = getRecommend(basket);
+    let avoidRecommends = getRecommend(avoidList);
 
-
-    recommendList = removeDoubles(recommendList);
+    console.log('after find candidates:');
     console.log(recommendList);
 
-    return [];
+    recommendList = removeDoubles(recommendList);
+    avoidRecommends = removeDoubles(avoidRecommends);
+
+    console.log('after rm doubles:');
+    console.log(recommendList);
+
+    recommendList = getArrayFromMeasureObjectArray(recommendList);
+    avoidRecommends = getArrayFromMeasureObjectArray(avoidRecommends);
+    avoidRecommends.splice(3);
+
+    console.log('after arraying:');
+    console.log(recommendList);
+
+    recommendList = recommendList.filter(x => !avoidRecommends.includes(x));
+
+    console.log('after set diffs:');
+    console.log(recommendList);
+
+    recommendList = removeSelf(recommendList, basket, avoidList);
+
+    console.log('after remove self:');
+    console.log(recommendList);
+
+    recommendList.splice(5);
+
+    console.log('after splice:');
+    console.log(recommendList);
+
+    return getFullInfo(recommendList);
 }
 
 const removeDoubles = function(list) {
@@ -22,9 +50,27 @@ const removeDoubles = function(list) {
         }
     });
 
-    return incs;
+    return res;
 }
 
-recommend();
+const getArrayFromMeasureObjectArray = function(arrayOfObjects) {
+    let res = [];
+    arrayOfObjects.forEach(obj => {
+        res.push(obj.name);
+    })
+
+    return res;
+}
+
+const removeSelf = function(recommendList, basket, avoidList) {
+    let res = [];
+    recommendList.forEach(recommend => {
+        if (!basket.includes(recommend) && !avoidList.includes(recommend)) {
+            res.push(recommend);
+        }
+    });
+
+    return res;
+}
 
 module.exports = recommend;
--- utils/content/recommend.js
+++ utils/content/recommend.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 
 const getRecommend = function(basket) {
+    if (basket.length === 0) {
+        return [];
+    }
+
     let measures = JSON.parse(fs.readFileSync('measures.json'));
 
-    let euqlidCandidates = [];
-    let diffsCandidates = [];
-    let treeCandidates = [];
     let candidates = [];
-
     measures.forEach(pair => {
-        let measure = pair.euclid / 2 + (1 - pair.diffs / 6) + (1 - pair.treeProx / 5);
 
         if (basket.includes(pair.first)) {
+            let measure = (1 - pair.euclid / 2) + (1 - pair.diffs / 6) + (1 - pair.treeProx / 5);
+
             candidates.push({
                 name: pair.second,
                 measure: measure
@@ -19,6 +20,8 @@ const getRecommend = function(basket) {
         }
 
         if (basket.includes(pair.second)) {
+            let measure = (1 - pair.euclid / 2) + (1 - pair.diffs / 6) + (1 - pair.treeProx / 5);
+            
             candidates.push({
                 name: pair.first,
                 measure: measure
